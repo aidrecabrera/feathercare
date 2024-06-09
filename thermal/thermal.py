44c8100d38cc12cf280434b053cfd52211e03c84
Svene <hello.svene@gmail.com>
2024-06-10 03:44:10 +0800

    thermal

--- thermal/thermal.py
+++ thermal/thermal.py
@@ -1,21 +1,20 @@
+import seeed_mlx9064x
 import time
-import board
-import busio
-import numpy as np
-import adafruit_mlx90640
-
-i2c = busio.I2C(board.SCL, board.SDA, frequency=400000) # setup I2C
-mlx = adafruit_mlx90640.MLX90640(i2c) # begin MLX90640 with I2C comm
-mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ # set refresh rate
-
-frame = np.zeros((24*32,)) # setup array for storing all 768 temperatures
-while True:
-    try:
-        mlx.getFrame(frame) # read MLX temperatures into frame var
-        break
-    except ValueError:
-        continue # if error, just read again
-
-# print out the average temperature from the MLX90640
-print('Average MLX90640 Temperature: {0:2.1f}C ({1:2.1f}F)'.\
-      format(np.mean(frame),(((9.0/5.0)*np.mean(frame))+32.0)))
+CHIP_TYPE = 'MLX90641'
+def main():
+    if CHIP_TYPE == 'MLX90641':
+        mlx = seeed_mlx9064x.grove_mxl90641()
+        frame = [0] * 192
+    elif CHIP_TYPE == 'MLX90640':
+        mlx = seeed_mlx9064x.grove_mxl90640()
+        frame = [0] * 768  
+    mlx.refresh_rate = seeed_mlx9064x.RefreshRate.REFRESH_8_HZ  # The fastest for raspberry 4 
+    time.sleep(1)
+    while True:
+        start = time.time()
+        mlx.getFrame(frame)
+        print(frame)
+        end = time.time()
+        print("The time: %f"%(end - start))
+if __name__  == '__main__':
+    main()
